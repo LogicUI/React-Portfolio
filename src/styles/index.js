f43b994bba081f63277641d070b145d4dd9204f0
john <johnwee35@gmail.com>
2019-06-18 23:57:01 +0800

    added background image

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,7 +1,10 @@
 import { makeStyles } from '@material-ui/styles';
+import background from '../images/background.jpeg';
 
 export const styles = makeStyles({
   root: {
     height: '100vh',
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover'
   }
 });
