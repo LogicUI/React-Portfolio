8cf796bf4a2469bcb440c814610ab00032255958
john <johnwee35@gmail.com>
2019-08-03 23:56:00 +0800

    move jss for aboutstyles to about component directly

--- src/components/navigations/About.js
+++ src/components/navigations/About.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import { withStyles } from '@material-ui/styles';
-import { aboutStyles } from '../../styles/aboutStyles';
+import { makeStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
-import profile from "../../images/profileimage.jpg";
 
+const aboutStyles = makeStyles({
+  root: {
+    height: '90vh',
+    width: '90vw',
+    borderRadius: '8px',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    textAlign: 'center'
+  },
+  bigAvatar: {
+    margin: 10,
+    width: 120,
+    height: 120
+  },
+  h2: {
+    color: 'palevioletred',
+    fontWeight: '500',
+    marginTop: '4rem',
+    fontSize: '4rem',
+    lineHeight: '1.5',
+    textTransform: 'uppercase'
+  }
+});
 
-
-const About = (props) => {
-  const { classes } = props;
+const About = () => {
+  const classes = aboutStyles();
   return (
     <section className={classes.root}>
-      <Typography variant="h2" gutterBottom centered classes={{h2:classes.h2}} >
+      <Typography
+        variant="h2"
+        gutterBottom
+        centered
+        classes={{ h2: classes.h2 }}
+      >
         About Me
       </Typography>
-      <Avatar alt="my photo" src={profile} className={classes.bigAvatar}  gutterBottom s/>
-    <Typography variant="h5" gutterBottom classes={{ h3: classes.h3 }}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-        </Typography>
+      <Typography variant="h5" gutterBottom classes={{ h3: classes.h3 }}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries,
+      </Typography>
     </section>
   );
 };
 
-export default withStyles(aboutStyles)(About);
+export default About;
