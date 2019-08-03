e73a1a325fd51dcf6b40ef42b4ddde1e8b1412f4
john <johnwee35@gmail.com>
2019-08-04 00:18:57 +0800

    update header.js component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,71 +1,79 @@
-import { AppBar, Toolbar, Tabs, Tab, Link } from '@material-ui/core';
-import { headerStyles } from '../styles/headerStyles';
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/styles';
+import { AppBar, Toolbar, Tabs, Tab } from '@material-ui/core';
+import React, { useState } from 'react';
 import { Home, Person, Folder, ImageSearch } from '@material-ui/icons';
 import { Link as RouterLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/styles';
 
-export class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0
-    };
+const headerStyles = makeStyles({
+  root: {
+    backgroundColor: '#1A1423'
+  },
+
+  MuiTabRoot: {
+    padding: '6px 2rem'
+  },
+
+  tool: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  indicator: {
+    backgroundColor: 'white'
   }
+});
 
-  handleOnChange = (event, newValue) => {
-    this.setState({
-      value: newValue
-    });
+const Header = () => {
+  const classes = headerStyles();
+  const [tabValue, setTabValue] = useState(0);
+
+  const handleOnChange = (event, newValue) => {
+    setTabValue(newValue);
   };
 
-  render() {
-    const { classes } = this.props;
+  return (
+    <AppBar position="fixed" className={classes.root}>
+      <Toolbar className={classes.tool}>
+        <Tabs
+          value={tabValue}
+          onChange={handleOnChange}
+          variant="scrollable"
+          scrollButtons="on"
+          classes={{ indicator: classes.indicator }}
+        >
+          <Tab
+            icon={<Home />}
+            label="Home"
+            component={RouterLink}
+            to="/"
+            classes={{ root: classes.MuiTabRoot }}
+          />
+          <Tab
+            icon={<Person />}
+            label="About"
+            component={RouterLink}
+            classes={{ root: classes.MuiTabRoot }}
+            to="/about"
+          />
 
-    return (
-      <AppBar position="fixed" className={classes.root}>
-        <Toolbar className={classes.tool}>
-          <Tabs
-            value={this.state.value}
-            onChange={this.handleOnChange}
-            variant="scrollable"
-            scrollButtons="on"
-            classes={{ indicator: classes.indicator }}
-          >
-            <Tab
-              icon={<Home />}
-              label="Home"
-              component={RouterLink}
-              to="/"
-              classes={{ root: classes.MuiTabRoot }}
-            />
-            <Tab
-              icon={<Person />}
-              label="About"
-              component={RouterLink}
-              classes={{ root: classes.MuiTabRoot }}
-              to="/about"
-            />
+          <Tab
+            icon={<ImageSearch />}
+            label="Skills"
+            to="/skills"
+            component={RouterLink}
+            classes={{ root: classes.MuiTabRoot }}
+          />
+          <Tab
+            icon={<Folder />}
+            label="Projects"
+            to="/projects"
+            component={RouterLink}
+            classes={{ root: classes.MuiTabRoot }}
+          />
+        </Tabs>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-            <Tab
-              icon={<ImageSearch />}
-              label="Skills"
-              to="/skills"
-              component={RouterLink}
-              classes={{ root: classes.MuiTabRoot }}
-            />
-            <Tab
-              icon={<Folder />}
-              label="Projects"
-              to="/projects"
-              component={RouterLink}
-              classes={{ root: classes.MuiTabRoot }}
-            />
-          </Tabs>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
-
-export default withStyles(headerStyles)(Header);
+export default Header;
