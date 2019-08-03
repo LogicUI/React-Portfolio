5070742fab2ebc220b4a077fed5c55c82986bd28
john <johnwee35@gmail.com>
2019-08-03 23:47:13 +0800

    Integrate jss changes to home.js

--- src/components/navigations/Home.js
+++ src/components/navigations/Home.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import { homeStyles } from '../../styles/homeStyles';
-import { withStyles } from '@material-ui/styles';
 import IconButton from '@material-ui/core/IconButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -9,9 +7,45 @@ import {
   faLinkedinIn,
   faFacebookF
 } from '@fortawesome/free-brands-svg-icons';
+import { makeStyles } from '@material-ui/styles';
 
-const Home = (props) => {
-  const { classes } = props;
+const homeStyles = makeStyles({
+  main: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    textAlign: 'center',
+    marginTop: '26px'
+  },
+  h2: {
+    color: 'lightcoral',
+    fontWeight: '500',
+    marginTop: '4rem',
+    fontSize: '4rem',
+    lineHeight: '1.5'
+  },
+  h3: {
+    color: 'white',
+    fontWeight: '100',
+    lineHeight: 1.5,
+    fontSize: '1.5rem'
+  },
+  buttons: {
+    marginTop: '2rem'
+  },
+  button: {
+    color: 'white',
+    fontSize: '32px',
+    padding: '1.5rem',
+    '&:hover': {
+      transform: 'scale(1.2)'
+    }
+  }
+});
+
+const Home = () => {
+  const classes = homeStyles();
 
   const headerDetails = () => {
     return (
@@ -20,8 +54,8 @@ const Home = (props) => {
           Hello, I'm John
         </Typography>
         <Typography variant="h3" gutterBottom classes={{ h3: classes.h3 }}>
-          I'm a software developer who is passion driven and an <br /> active
-          lifelong learner
+          I'm a full stack software developer who is passion driven and an{' '}
+          <br /> active lifelong learner
         </Typography>
       </section>
     );
@@ -51,4 +85,4 @@ const Home = (props) => {
   );
 };
 
-export default withStyles(homeStyles)(Home);
+export default Home;
