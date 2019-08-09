e38ab52b1c08c8e1aef8d5865a1917ccaf7f6e77
john <johnwee35@gmail.com>
2019-08-09 20:38:58 +0800

    update nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,56 +1,26 @@
 import React, { useState, Fragment } from 'react';
-import { darkBlue, white } from '../../helper/colorPalette';
-import { AppBar, Toolbar, Drawer } from '@material-ui/core';
+import { darkBlue } from '../../helper/colorPalette';
+import { AppBar, Toolbar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import IconButton from '@material-ui/core/IconButton';
-import SideList from '../SideList/SideList';
+import MenuBar from '../MenuBar/MenuBar';
 import TabBar from '../TabBar/TabBar';
 
 const styles = makeStyles((theme) => {
   return {
     root: {
       backgroundColor: darkBlue
-    },
-    menu: {
-      [theme.breakpoints.up('sm')]: {
-        display: 'none'
-      }
     }
   };
 });
 
 const Nav = () => {
   const classes = styles();
-  const [drawerState, toggleDrawerState] = useState(false);
-
-  const toggleDrawer = (state) => (event) => {
-    toggleDrawerState(state);
-  };
-
-  const menuBar = () => {
-    return (
-      <Fragment>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menuButton"
-          className={classes.menu}
-          onClick={toggleDrawer(true)}
-        >
-          <MenuIcon data-testid="menuIcon" />
-        </IconButton>
-        <Drawer amchor="left" open={drawerState} onClose={toggleDrawer(false)}>
-          <SideList />
-        </Drawer>
-      </Fragment>
-    );
-  };
+  const NavNames = ['Home', 'About', 'Projects', 'Contact Me'];
 
   return (
     <AppBar position="fixed" className={classes.root}>
       <Toolbar>
-        {menuBar()}
+        <MenuBar links={NavNames} />
         <TabBar />
       </Toolbar>
     </AppBar>
--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -6,27 +6,6 @@ import { createMemoryHistory } from 'history';
 import { Router } from 'react-router';
 import Nav from './Nav';
 
-describe('menu', () => {
-  let history;
-  beforeEach(() => {
-    history = createMemoryHistory({ initialEntries: ['/'] });
-  });
-  it('should be able to render the menu button correctly', () => {
-    const { getByLabelText } = render(
-      <Router history={history}>
-        <Nav />
-      </Router>
-    );
-
-    expect(getByLabelText('menuButton')).toBeInTheDocument();
-  });
-
-  it('should be able to render the menu icon correctly', () => {
-    const { getByTestId } = render(
-      <Router history={history}>
-        <Nav />
-      </Router>
-    );
-    expect(getByTestId('menuIcon')).toBeInTheDocument();
-  });
+xit('should ', () => {
+    
 });
