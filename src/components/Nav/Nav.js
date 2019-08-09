ec1ac62929cb72af23e6bc624b1d9d1df6d8f98b
john <johnwee35@gmail.com>
2019-08-09 16:58:30 +0800

    edited nav.js to refactor out tabs to its own component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,22 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { darkBlue, white } from '../../helper/colorPalette';
-import { AppBar, Toolbar, Tabs, Tab } from '@material-ui/core';
+import { AppBar, Toolbar, Drawer } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
+import SideList from '../SideList/SideList';
+import TabBar from '../TabBar/TabBar';
 
 const styles = makeStyles((theme) => {
   return {
     root: {
       backgroundColor: darkBlue
-    },
-    indicator: {
-      backgroundColor: white
-    },
-    links: {
-      [theme.breakpoints.down('xs')]: {
-        display: 'none'
-      }
     },
     menu: {
       [theme.breakpoints.up('sm')]: {
@@ -28,41 +22,28 @@ const styles = makeStyles((theme) => {
 
 const Nav = () => {
   const classes = styles();
+  const [drawerState, toggleDrawerState] = useState(false);
 
-  const [tabValue, setTabValue] = useState(0);
-
-  const handleOnChange = (event, newValue) => {
-    setTabValue(newValue);
-  };
-
-  const tabLinks = () => {
-    return (
-      <Tabs
-        value={tabValue}
-        onChange={handleOnChange}
-        variant="scrollable"
-        scrollButtons="on"
-        classes={{ indicator: classes.indicator }}
-        className={classes.links}
-      >
-        <Tab label="Home" />
-        <Tab label="About" />
-        <Tab label="Projects" />
-        <Tab label="Contact Me" />
-      </Tabs>
-    );
+  const toggleDrawer = (state) => (event) => {
+    toggleDrawerState(state);
   };
 
   const menuBar = () => {
     return (
-      <IconButton
-        edge="start"
-        color="inherit"
-        aria-label="menuButton"
-        className={classes.menu}
-      >
-        <MenuIcon data-testid="menuIcon" />
-      </IconButton>
+      <Fragment>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menuButton"
+          className={classes.menu}
+          onClick={toggleDrawer(true)}
+        >
+          <MenuIcon data-testid="menuIcon" />
+        </IconButton>
+        <Drawer amchor="left" open={drawerState} onClose={toggleDrawer(false)}>
+          <SideList />
+        </Drawer>
+      </Fragment>
     );
   };
 
@@ -70,7 +51,7 @@ const Nav = () => {
     <AppBar position="fixed" className={classes.root}>
       <Toolbar>
         {menuBar()}
-        {tabLinks()}
+        <TabBar />
       </Toolbar>
     </AppBar>
   );
--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -6,68 +6,27 @@ import { createMemoryHistory } from 'history';
 import { Router } from 'react-router';
 import Nav from './Nav';
 
-describe('Nav component', () => {
-  describe('Links', () => {
-    let history;
-    beforeEach(() => {
-      history = createMemoryHistory({ initialEntries: ['/'] });
-    });
+describe('menu', () => {
+  let history;
+  beforeEach(() => {
+    history = createMemoryHistory({ initialEntries: ['/'] });
+  });
+  it('should be able to render the menu button correctly', () => {
+    const { getByLabelText } = render(
+      <Router history={history}>
+        <Nav />
+      </Router>
+    );
 
-    it('should be able to render the home link', () => {
-      const { getByText } = render(
-        <Router history={history}>
-          <Nav />
-        </Router>
-      );
-      expect(getByText('Home')).toBeInTheDocument();
-    });
+    expect(getByLabelText('menuButton')).toBeInTheDocument();
+  });
 
-    it('should be able to render the about link', () => {
-      const { getByText } = render(
-        <Router history={history}>
-          <Nav />
-        </Router>
-      );
-      expect(getByText('About')).toBeInTheDocument();
-    });
-
-    it('should be able to render the Projects link', () => {
-      const { getByText } = render(
-        <Router history={history}>
-          <Nav />
-        </Router>
-      );
-      expect(getByText('Projects')).toBeInTheDocument();
-    });
-
-    it('should be able to render the Contact Me link', () => {
-      const { getByText } = render(
-        <Router history={history}>
-          <Nav />
-        </Router>
-      );
-      expect(getByText('Contact Me')).toBeInTheDocument();
-    });
-
-    describe('menu', () => {
-      it('should be able to render the menu button correctly', () => {
-        const { getByLabelText } = render(
-          <Router history={history}>
-            <Nav />
-          </Router>
-        );
-
-        expect(getByLabelText('menuButton')).toBeInTheDocument();
-      });
-
-      it('should be able to render the menu icon correctly', () => {
-        const { getByTestId } = render(
-          <Router history={history}>
-            <Nav />
-          </Router>
-        );
-        expect(getByTestId('menuIcon')).toBeInTheDocument();
-      });
-    });
+  it('should be able to render the menu icon correctly', () => {
+    const { getByTestId } = render(
+      <Router history={history}>
+        <Nav />
+      </Router>
+    );
+    expect(getByTestId('menuIcon')).toBeInTheDocument();
   });
 });
